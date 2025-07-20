0a70c6d398bb67e79aebc565f3a864878970b7ca
kenji10190 <oinari13579@gmail.com>
2025-07-20 23:30:18 +0900

    Add test code for user registration

--- test/app.test.js
+++ test/app.test.js
@@ -1,22 +1,40 @@
 const request = require("supertest");
-const { express } = require("chai");
+const { expect } = require("chai");
 const app = require("../app.js");
 
 const testUser = {
-  name: "testuser",
-  email: `testuser${Date.now()@example.com}`,
+  name: "testUser",
+  email: `testuser${Date.now()}@mail.com`,
   password: "password1234"
 }
 
 describe("ユーザー登録APIテスト", () => {
-  it("正しい情報で登録できるか", (done) => {
-    request(app)
+
+  let agent;
+  let csrfToken;
+
+  beforeEach(async () => {
+    agent = request.agent(app);
+  });
+
+  it("正しい情報で登録可能か", async () => {
+    const getResponse = await agent
+      .get("/register")
+      .expect(200);
+
+    const csrfMatch = getResponse.text.match(/<input type="hidden" name="_csrf" value="([^"]+)"/);
+    csrfToken = csrfMatch ? csrfMatch[0] : null;
+
+    if (!csrfToken) throw new Error("csrfTokenが見つかりません。");
+
+    const postResponse = await agent
       .post("/register")
-      .send(testUser)
-      .expect(302)
-      .end((err, response) => {
-        if(err) return done(err);
-        
+      .send({
+        ...testUser,
+        _csrf: csrfToken
       })
+      .expect(302);
+
+    expect(postResponse.headers.location).to.exist;
   })
 })
