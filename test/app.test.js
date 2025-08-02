01056d049668f46548e190a8e545111af2f0f6e1
kenji10190 <oinari13579@gmail.com>
2025-08-02 18:10:05 +0900

    fix test code

--- test/app.test.js
+++ test/app.test.js
@@ -12,42 +12,36 @@ describe("掲示板アプリ基本テスト", () => {
 
   let agent;
 
-  beforeEach(async () => {
+  beforeEach(() => {
     agent = request.agent(app);
   });
 
   describe("起動確認", () => {
     it("アプリが起動するか", async () => {
-      const getResponse = await agent
-        .get("/")
-        .expect(getResponse.status).to.equal(200);
+      const getResponse = await agent.get("/");
+      expect(getResponse.status).to.equal(200);
     })
   })
 
   describe("主要ページ表示確認", () => {
     it("トップページ表示確認", async () => {
-      const getResponse = await agent
-        .get("/")
-        .expect(getResponse.status).to.equal(200);
+      const getResponse = await agent.get("/");
+      expect(getResponse.status).to.equal(200);
     });
 
     it("ログインページ確認", async () => {
-      const getResponse = await agent
-        .get("/login")
-        .expect(getResponse.status).to.equal(200);
+      const getResponse = await agent.get("/login");
+      expect(getResponse.status).to.equal(200);
     });
 
     it("ユーザー登録ページ確認", async () => {
-      const getResponse = await agent
-        .get("/register")
-        .expect(getResponse.status).to.equal(200);
+      const getResponse = await agent.get("/register");
+      expect(getResponse.status).to.equal(200);
     });
   });
 
   it("正しい情報で登録可能か", async () => {
-    const getResponse = await agent
-      .get("/register")
-      .expect(200);
+    const getResponse = await agent.get("/register").expect(200);
 
     const csrfMatch = getResponse.text.match(/<input type="hidden" name="_csrf" value="([^"]+)"/);
     csrfToken = csrfMatch ? csrfMatch[1] : null;
